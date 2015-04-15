b1a95ab85c965afc3896d7ea0a377ec8cd090651
tomhof <tomhof@man.poznan.pl>
2015-04-15 11:09:40 +0200

    added constMN tester script

---
+++ gpe/constMN/gpeConstMN.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./gpe_tester --exp_mode=const_m_exp --e=3 --alpha1=1 --alpha2=2 --beta1=1 --beta2=2 --out_file=constMex03
+
+./gpe_tester --exp_mode=const_m_exp --e=4 --alpha1=1 --alpha2=2 --beta1=1 --beta2=2 --out_file=constMex04
+
+./gpe_tester --exp_mode=const_m_exp --e=8 --alpha1=1 --alpha2=2 --beta1=1 --beta2=2 --out_file=constMex08
